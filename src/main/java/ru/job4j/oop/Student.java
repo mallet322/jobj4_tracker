529da0910386ac2db3045069a343ba017dffa961
Илья Шокин <mallet322@gmail.com>
2021-08-26 13:41:35 +0300

    1.2. Вызов метода объекта [#166085]

---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Student {
+
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student elias = new Student();
+        elias.song();
+    }
+
+}
