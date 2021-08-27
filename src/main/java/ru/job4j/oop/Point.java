cd6ec39b12bb2f09637199db9ae0e83a5a501df0
Илья Шокин <mallet322@gmail.com>
2021-08-27 11:16:44 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/main/java/ru/job4j/oop/Point.java
+++ src/main/java/ru/job4j/oop/Point.java
@@ -2,23 +2,53 @@ package ru.job4j.oop;
 
 public class Point {
 
-    private double x;
-    private double y;
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private final int x;
 
-    public Point(double x, double y) {
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private final int y;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int z;
+
+    /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     * @param first координата x
+     * @param second координата y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    /**
+     * Конструктор, который принимает начальное состояние объекта "точка"
+     * @param x координата x
+     * @param y координата y
+     * @param z координата z
+     */
+    public Point(int x, int y, int z) {
         this.x = x;
         this.y = y;
+        this.z = z;
     }
 
     public double distance(Point that) {
         return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
     }
 
-    public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(0, 2);
-        double dist = a.distance(b);
-        System.out.println(dist);
+    public double distance3d(Point that) {
+        return distance(that) + Math.sqrt(Math.pow(this.z - that.z, 2));
+    }
+
+    public void info() {
+        System.out.println("Point[" + this.x + ", " + this.y + "]");
     }
 
 }
