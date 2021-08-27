ff82f44a6f701963df252bb1cb077e0b2796c31b
Илья Шокин <mallet322@gmail.com>
2021-08-27 08:51:24 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void whenDistance00And01Then1 () {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 1);
+        double expected = 1.0;
+        double actual = a.distance(b);
+        Assert.assertEquals(expected, actual, 0.001);
+    }
+
+    @Test
+    public void whenDistance00And02Then2 () {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double expected = 2.0;
+        double actual = a.distance(b);
+        Assert.assertEquals(expected, actual, 0.001);
+    }
+
+    @Test
+    public void whenDistance00And03Then3 () {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 3);
+        double expected = 3.0;
+        double actual = a.distance(b);
+        Assert.assertEquals(expected, actual, 0.001);
+    }
+
+}
