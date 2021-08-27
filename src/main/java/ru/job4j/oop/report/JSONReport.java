d0a7293b23c14d8fb42132e00358da4b78ec7a43
Илья Шокин <mallet322@gmail.com>
2021-08-27 15:13:53 +0300

    5. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/oop/report/JSONReport.java
+++ src/main/java/ru/job4j/oop/report/JSONReport.java
@@ -4,9 +4,9 @@ public class JSONReport extends TextReport {
 
     @Override
     public String generate(String name, String body) {
-        return "{ \n" +
-                "\"name\"" + " : \"" + name + "\"," + "\n" +
-                "\"body\"" + " : \"" + body + "\"" + "\n" +
+        return "{" + System.lineSeparator() +
+                "\"name\"" + " : \"" + name + "\"," + System.lineSeparator() +
+                "\"body\"" + " : \"" + body + "\"" + System.lineSeparator() +
                "}";
     }
 
