492a1c5706e33e2ce97a4815a5a511e138f82836
Илья Шокин <mallet322@gmail.com>
2021-08-27 08:36:31 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,28 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        another.load = this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery battery = new Battery(100);
+        Battery another = new Battery(0);
+        battery.exchange(another);
+        printLoad(battery);
+        printLoad(another);
+    }
+
+    private static void printLoad(Battery battery) {
+        System.out.println("Load: " + battery.load);
+    }
+
+}
