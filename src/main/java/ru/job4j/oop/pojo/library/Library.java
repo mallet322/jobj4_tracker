acf7cb4a15b07b99318bd4f048964afcac0d231f
Илья Шокин <mallet322@gmail.com>
2021-08-28 11:21:47 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/oop/pojo/library/Library.java
@@ -0,0 +1,38 @@
+package ru.job4j.oop.pojo.library;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book book0 = new Book("Head first Java", 300);
+        Book book1 = new Book("The philosophy of java", 500);
+        Book book2 = new Book("Clean code", 1000);
+        Book book3 = new Book("Spring in action", 350);
+        Book[] books = new Book[4];
+        books[0] = book0;
+        books[1] = book1;
+        books[2] = book2;
+        books[3] = book3;
+        System.out.println("Show books info:");
+        for (Book book : books) {
+            System.out.println("Book name: " + book.getName() + ", Pages count: " + book.getPageCount());
+        }
+        swap(books, 0, 3);
+        System.out.println(System.lineSeparator() + "Show books info after swap elements:");
+        for (Book book : books) {
+            System.out.println("Book name: " + book.getName() + ", Pages count: " + book.getPageCount());
+        }
+        System.out.println(System.lineSeparator() + "Show books with \"Clean code name\"");
+        for (Book book : books) {
+            if ("Clean code".equals(book.getName())) {
+                System.out.println("Book name: " + book.getName() + ", Pages count: " + book.getPageCount());
+            }
+        }
+    }
+
+    private static void swap(Book[] books, int source, int dest) {
+        Book temp = books[source];
+        books[source] = books[dest];
+        books[dest] = temp;
+    }
+
+}
