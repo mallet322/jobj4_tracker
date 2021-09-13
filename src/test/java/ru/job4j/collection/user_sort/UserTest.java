cb56ec5f4fc9283a216657a8d3ff6b12babce251
Илья Шокин <mallet322@gmail.com>
2021-09-13 09:25:03 +0300

    1. Организовать сортировку User [#10034]

---
+++ src/test/java/ru/job4j/collection/user_sort/UserTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection.user_sort;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTest {
+
+    @Test
+    public void whenAsc() {
+        Set<User> users = new TreeSet<>();
+        users.add(new User("Petr", 32));
+        users.add(new User("Ivan", 31));
+        Iterator<User> it = users.iterator();
+        Assert.assertThat(it.next(), Matchers.is(new User("Ivan", 31)));
+        Assert.assertThat(it.next(), Matchers.is(new User("Petr", 32)));
+    }
+
+    @Test
+    public void whenComparePertVSIvan() {
+        int rsl = new User("Petr", 32).compareTo(new User("Ivan", 31));
+        Assert.assertThat(rsl, Matchers.greaterThan(0));
+    }
+
+}
