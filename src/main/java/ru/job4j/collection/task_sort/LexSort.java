2a295522640c18eb55f60dcba1cc32b4bb620f91
Илья Шокин <mallet322@gmail.com>
2021-09-13 14:35:30 +0300

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/task_sort/LexSort.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection.task_sort;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        int leftNumber = Integer.parseInt(left.split("\\.")[0]);
+        int rightNumber = Integer.parseInt(right.split("\\.")[0]);
+        return Integer.compare(leftNumber, rightNumber);
+    }
+
+}
