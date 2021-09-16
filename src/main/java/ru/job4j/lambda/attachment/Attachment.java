9088f96328340ae3b48b86af4decb8c4501389c8
Илья Шокин <mallet322@gmail.com>
2021-09-16 14:29:27 +0300

    1.1. Анонимные классы [#214142]

---
+++ src/main/java/ru/job4j/lambda/attachment/Attachment.java
@@ -0,0 +1,29 @@
+package ru.job4j.lambda.attachment;
+
+public class Attachment {
+
+    private final String name;
+    private final int size;
+
+    public Attachment(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", size=" + size +
+                '}';
+    }
+
+}
