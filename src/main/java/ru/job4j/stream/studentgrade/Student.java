3115e65d0e0f8a8387b754c5ee3c056a18718c8e
Илья Шокин <mallet322@gmail.com>
2021-09-21 11:21:47 +0300

    0. Stream API улучшения [#58167]

---
+++ src/main/java/ru/job4j/stream/studentgrade/Student.java
@@ -0,0 +1,49 @@
+package ru.job4j.stream.studentgrade;
+
+import java.util.Objects;
+
+public class Student {
+
+    private final int score;
+    private String surname;
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Student(String surname, int score) {
+        this.score = score;
+        this.surname = surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return score == student.score &&
+                surname.equals(student.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(score, surname);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{"
+                + "score=" + score
+                + ", surname='" + surname + '\''
+                + '}';
+    }
+
+}
