887295ad915a9f24b4ffc19b39b9ab7f4086d70e
Илья Шокин <mallet322@gmail.com>
2021-09-22 14:55:40 +0300

    3. Optional в банковских переводах. [#214711]

---
+++ src/test/java/ru/job4j/stream/college/CollegeTest.java
@@ -0,0 +1,104 @@
+package ru.job4j.stream.college;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CollegeTest {
+
+    @Test
+    public void whenAccountIsOptionalEmpty() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        Assert.assertThat(college.findByAccount("000010"), Matchers.is(Optional.empty()));
+    }
+
+    @Test
+    public void whenAccountIsOptionalNotEmpty() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        college.findByAccount("000001")
+               .ifPresent(student -> Assert.assertThat(student.getGroup(), Matchers.is("201-18-15")));
+    }
+
+    @Test
+    public void whenSubjectIsOptionalEmptyWithNotFoundAccount() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        Assert.assertThat(college.findBySubjectName("000010", "Sociology"), Matchers.is(Optional.empty()));
+    }
+
+    @Test
+    public void whenSubjectIsOptionalEmptyWithFoundAccount() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        Assert.assertThat(college.findBySubjectName("000001", "Sociology"), Matchers.is(Optional.empty()));
+    }
+
+    @Test
+    public void whenSubjectIsOptionalNotEmpty() {
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student1", "000001", "201-18-15"),
+                Set.of(
+                        new Subject("Math", 70),
+                        new Subject("English", 85)
+                ),
+                new Student("Student2", "000002", "201-18-15"),
+                Set.of(
+                        new Subject("Economic", 75),
+                        new Subject("Sociology", 65)
+                )
+        );
+        College college = new College(students);
+        Assert.assertThat(college.findBySubjectName("000002", "Sociology").get().getScore(), Matchers.is(65));
+    }
+
+}
