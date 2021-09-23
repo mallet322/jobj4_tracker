ebd6f4fdeca5e3e051597495fb488bd305e8535a
Илья Шокин <mallet322@gmail.com>
2021-09-23 08:52:26 +0300

    4. Optional в Stream API. [#279130] - fix

--- src/test/java/ru/job4j/collection/bank/BankServiceTest.java
+++ src/test/java/ru/job4j/collection/bank/BankServiceTest.java
@@ -22,7 +22,7 @@ public class BankServiceTest {
         BankService bank = new BankService();
         bank.addUser(user);
         bank.addAccount(user.getPassport(), new Account("5546", 150D));
-        Assert.assertNull(bank.findByRequisite("34", "5546"));
+        Assert.assertEquals(bank.findByRequisite("34", "5546"), Optional.empty());
     }
 
     @Test
